5b1e2c442b83aca36c6f35fb79520240fb7230d9
wotkad <wotkad@gmail.com>
2024-02-02 18:16:16 +0300

    update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -13,7 +13,4 @@ import "./base/common/preloader";
 import "./base/common/smooth-scroll";
 import "./base/common/scroll-reveal";
 
-import "./base/checks/check-internal-links";
-import "./base/checks/check-target";
 import "./base/common/get-current-year";
-import "./base/common/get-header-height";
