d43fd671a8511904f52fac80cf136cdb1acdf0a3
wotkad <wotkad@gmail.com>
2024-02-21 19:23:28 +0300

    update mobile

--- src/assets/js/components/slider/slider-reviews.js
+++ src/assets/js/components/slider/slider-reviews.js
@@ -5,14 +5,18 @@ export default function sliderReviews() {
   new Swiper('.reviews__slider', {
     modules: [ Mousewheel ],
     speed: 800,
-    slidesPerView: 2.5,
+    slidesPerView: 1.2,
     spaceBetween: 20,
     grabCursor: true,
     mousewheel: {
       forceToAxis: true,
     },
     breakpoints: {
+      768: {
+        slidesPerView: 2.5,
+      },
       1224: {
+        slidesPerView: 2.5,
         spaceBetween: 24,
       }
     }
