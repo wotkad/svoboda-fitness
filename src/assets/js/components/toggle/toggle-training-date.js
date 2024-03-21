6c29cb60eadf49a07eaf03ff5cfdcc889d34104e
wotkad <wotkad@gmail.com>
2024-03-21 19:39:35 +0300

    fix

--- src/assets/js/components/toggle/toggle-training-date.js
+++ src/assets/js/components/toggle/toggle-training-date.js
@@ -6,7 +6,7 @@ export default function toggleTrainingDate() {
       let date = $(this).siblings('.schedule-table__head').find('.schedule-table__date').text();
       let id = button[i].getAttribute('data-popup-button');
       let block = $('.popup[data-popup="' + id + '"]');
-      $(block).attr('data-id', $(this).attr('data-id')).find('.popup__date').text(`${day} ${date}`);
+      $(block).attr('data-id', $(this).attr('data-id')).find('.popup__date').text(`${day}, ${date}`);
     });
   }
 }
